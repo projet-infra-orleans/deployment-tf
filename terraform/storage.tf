1d7fa8c92774168c27eff5fb586d8ffa26ae4fc8
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-10 14:56:14 +0100

    Update storage.tf

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -1,15 +1,13 @@
-resource "random_id" "storage" {
-    byte_length = 8
-}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
-resource "azurerm_storage_account" "example" {
-  name                     = lower("${var.prefix}${random_id.storage.id}")
+resource "azurerm_storage_account" "gr1infradeploystock" {
+  name                     = lower("${var.stockage}")
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "RA-GRS"
 
   tags = {
     environment = "staging"
   }
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+ 
