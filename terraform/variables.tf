7964c7dd5a35b2de7c6495b00d4eec13a881b57a
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-10 16:32:39 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,11 +6,14 @@ variable "stockage_name" {
     type = string
 }
 
+variable "cluster_name" {
+    type = string
+}
+
 variable "resource_group_location" {
     type = string
     default = "West Europe"
 }
-
 
 variable "environment" {
     type = string
