13a8a2eae186f054f3e048a2d19939ebcb35058e
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-10 15:01:08 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,9 +5,18 @@ terraform {
       version = "3.73.0"
     }
   }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.23.0"
+    }
 }
 
 provider "azurerm" {
   features {}
   skip_provider_registration = true 
+}
+
+
+provider "kubernetes" {
+  # Configuration options
 }
