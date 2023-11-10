94a683228eb97ac2c467af14def172e9af4b3f4a
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-10 14:57:02 +0100

    Update rg.tf

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "groupe-1-infra-deploy" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
