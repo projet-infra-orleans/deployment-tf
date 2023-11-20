81c448b62e653c844f88d06f5d26f10dd806d526
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-20 14:54:52 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,6 +22,24 @@ terraform {
 
 }
 
+# Utilisation d'une ressource de groupe existante
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+# Utilisation d'un compte de stockage existant
+data "azurerm_storage_account" "stockage" {
+  name                = "${var.stockage_name}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+# Utilisation d'un cluster AKS existant
+data "azurerm_kubernetes_cluster" "aks" {
+  name                =  "${var.cluster_name}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+
 provider "azurerm" {
   features {}
   skip_provider_registration = true 
