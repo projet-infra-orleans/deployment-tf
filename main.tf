956486b1cf16006a15ea910edddf90202b02f587
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-20 16:03:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -76,3 +76,4 @@ provider "kubernetes" {
   client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
 }
+
