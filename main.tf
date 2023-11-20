355e783d21d6aca2f05d7cebd8ee532a4c0b3b0c
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-20 15:54:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,8 +51,16 @@ provider "azurerm" {
   skip_provider_registration = true 
 }
 
+provider "kubectl" {
+  host                   = data.azurerm_kubernetes_cluster.aks.kube_config[0].host
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
+  load_config_file       = false
+}
 
 provider "helm" {
+  debug   = true
   kubernetes {
     /* config_path = "~/.kube/config" */
   host = data.azurerm_kubernetes_cluster.aks.kube_config.0.host
