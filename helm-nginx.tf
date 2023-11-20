848bf53ad29f02036a2fe191834307026175dda7
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-20 16:04:54 +0100

    Update helm-nginx.tf

--- helm-nginx.tf
+++ helm-nginx.tf
@@ -1,11 +1,17 @@
-resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress-controller"
+resource "helm_release" "ingress" {
+  depends_on       = [azurerm_key_vault_access_policy.aks_access_to_kv]
+  name             = "${local.prefix}-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx/"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-ns"
+  create_namespace = true
+}
 
-  repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
-
-  set {
-    name  = "service.type"
-    value = "ClusterIP"
-  }
+resource "helm_release" "akv2k8" {
+  depends_on       = [helm_release.ingress]
+  name             = "${local.prefix}-akv2k8"
+  repository       = "https://charts.spvapi.no"
+  chart            = "akv2k8s"
+  namespace        = "akv2k8s"
+  create_namespace = true
 }
