e3e4ce39e48915cf49a503b99fb1253bfdd18ca0
FatouDllo <72863186+FatouDllo@users.noreply.github.com>
2023-11-22 10:29:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,12 +9,12 @@ terraform {
 
     helm = {
       source = "hashicorp/helm"
-      version = "2.11.0"
+      version = "2.6.0"
     }
     
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
+      version = ">= 1.14.0"
     }
 
     kubernetes = {
@@ -75,5 +75,62 @@ provider "kubernetes" {
   client_certificate     = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
   client_key             = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
   cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
+
+data "azurerm_client_config" "current" {}
+
+# Utilisation d'un coffre de clés existant
+data "azurerm_key_vault" "tf_kv" {
+  depends_on          = [data.azurerm_kubernetes_cluster.aks]
+  name                = "${var.vault_key_name}"
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_key_vault_access_policy" "aks_access_to_kv" {
+  depends_on       = [data.azurerm_kubernetes_cluster.aks]
+  key_vault_id     = data.azurerm_key_vault.tf_kv.id
+  tenant_id        = data.azurerm_client_config.current.tenant_id
+  object_id        = data.azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+
+  certificate_permissions = [
+    "Get",
+  ]
+
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+resource "helm_release" "ingress" {
+  depends_on       = [azurerm_key_vault_access_policy.aks_access_to_kv]
+  name             = "ingress-nginx"
+  repository       = "https://kubernetes.github.io/ingress-nginx/"
+  chart            = "ingress-nginx"
+  namespace        = "ingress-ns"
+  create_namespace = true
+}
+
+resource "helm_release" "akv2k8" {
+  depends_on       = [helm_release.ingress]
+  name             = "ingress-akv2k8"
+  repository       = "https://charts.spvapi.no"
+  chart            = "akv2k8s"
+  namespace        = "akv2k8s"
+  create_namespace = true
+}
+
+resource "kubectl_manifest" "create_namespace" {
+  depends_on = [helm_release.akv2k8] 
+  yaml_body  = file("./manifests/createNamespace.yaml")
+}
+
+resource "kubectl_manifest" "sync_cert_service" {
+  depends_on = [kubectl_manifest.create_namespace] 
+  yaml_body  = file("./manifests/syncCert.yaml")
+}
+
+resource "kubectl_manifest" "use_cert_service" {
+  depends_on = [kubectl_manifest.sync_cert_service] 
+  yaml_body  = file("./manifests/useCert.yaml")
 }
 
